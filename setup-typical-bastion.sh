77522652bc850c06f80f94a8029d09bb417bd333
Alexandr Sabitov <a.sabitov@hotmail.com>
2018-06-06 07:16:05 +0000

    added bind-utils

--- setup-typical-bastion.sh
+++ setup-typical-bastion.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 yum -y install epel-release
-yum -y install strace tcpdump vim mc wget git curl jq
+yum -y install strace tcpdump strace vim mc wget git curl jq bind-utils
 yum -y install python34 python34-pip
 
 pip3 install awscli --upgrade --user
