2c10cdb80800dd9f16163474977af5c6e0158a18
Alexandr Sabitov <a.sabitov@hotmail.com>
2018-06-29 08:22:24 +0000

    added ssh alive

--- setup-typical-bastion.sh
+++ setup-typical-bastion.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
+### Install essential software
+
 yum -y install epel-release
 yum -y install telnet strace tcpdump strace vim mc wget git curl jq bind-utils mariadb docker
 yum -y install python34 python34-pip
 
-pip3 install awscli --upgrade --user
-export PATH=$PATH:/root/.local/bin/
-sed -i 's/^PATH=$PATH:$HOME\/bin$/PATH=$PATH:$HOME\/bin:\/root\/.local\/bin/' ~/.bash_profile
+### Change host's name
 
 INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
 REGION=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | grep region | awk -F\" '{print $4}')
+
 
 BASTION_NAME=$(aws ec2 describe-tags --filters "Name=resource-id,Values=$INSTANCE_ID" --region=$REGION --output=text | cut -f5)
 
 if [ -z $BASTION_NAME ]; then
   BASTION_NAME="bastion"
-fi  
+fi
 
 hostnamectl set-hostname --static $BASTION_NAME
 
@@ -23,13 +24,33 @@ if ! grep preserve_hostname /etc/cloud/cloud.cfg; then
   echo "preserve_hostname: true" >> /etc/cloud/cloud.cfg
 else
   sed -i 's/.*preserve_hostname:.*/preserve_hostname: true/' /etc/cloud/cloud.cfg
-fi 
+fi
+
+### Install AWS CLI
+
+pip3 install awscli --upgrade --user
+export PATH=$PATH:/root/.local/bin/
+sed -i 's/^PATH=$PATH:$HOME\/bin$/PATH=$PATH:$HOME\/bin:\/root\/.local\/bin/' ~/.bash_profile
+
+### Keep SSH alive
+
+sed -i 's/.*ClientAliveInterval.*/ClientAliveInterval 120/' /etc/ssh/sshd_config
+sed -i 's/.*ClientAliveCountMax.*/ClientAliveCountMax 720/' /etc/ssh/sshd_config
+systemctl restart sshd
+
+### Create essential folders
 
 mkdir -p /root/{git,tmp}
 
+### Configure Git
+
 git config --global push.default simple
+
+### Enable Docker
 
 systemctl enable docker
 systemctl start docker
+
+### Reboot
 
 echo "Please don't forget to reboot!" 
